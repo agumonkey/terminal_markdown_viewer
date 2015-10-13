46fb3f475e03ac6a49008fa4a28e16258adec166
agumonkey <agumonkey@gmail.com>
2015-10-13 14:30:24 +0200

    deprecated mdv.core.logger module

--- mdv/core/logger.py
+++ mdv/core/logger.py
@@ -2,9 +2,14 @@
 fake interim loggers
 '''
 
+raise Exception('This module is deprecated. Use logging.')
 
 def info(*msg):
     print(*('[info]',) + msg)
+
+
+def debug(*msg):
+    print(*('[debug]',) + msg)
 
 
 def warn(*msg):
